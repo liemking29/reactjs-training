46217522c131b8bf6d562ea155749d911aa63447
liemking29 <liemking2902@gmail.com>
2025-09-08 17:32:07 +0700

    The first reactjs project

---
+++ reactjs-08092025/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-800">Xin chào!</h1>
+    </div>
+  );
+}
+
+export default App;
